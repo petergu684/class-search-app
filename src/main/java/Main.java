44f77318f828559941e4e270f5910ab125359605
Wenhao Gu <petergu684@gmail.com>
2021-01-28 01:10:34 -0500

    Implemented basic query with API Key

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("JHU - WSE - CS Dpr.");
+        System.out.println("JHU - WSE - CS Dpt.");
         System.out.println("Spring 2021");
         System.out.println("Class Search");
         System.out.println("Enter your search query");
@@ -14,6 +18,11 @@ public class Main {
     }
 
     private static void search(String query) {
-        System.out.println("Search not implemented yet!");
+        final String BASE_URL = "https://sis.jhu.edu/api/classes";
+        final String KEY = System.getenv("JHU_API_KEY");
+
+        String endpoint = BASE_URL + "/codes/schools?Key=" + KEY;
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(endpoint).asJson();
+        System.out.println(jsonResponse.getBody().toString());
     }
 }
